d7f694130be8e0c2fc0e058d37f6c37d116e04c7
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 06:45:53 +0700

    [feat]: set up bottom navigation

--- app/src/main/java/com/reift/kultum/MainActivity.kt
+++ app/src/main/java/com/reift/kultum/MainActivity.kt
@@ -2,12 +2,23 @@ package com.reift.kultum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.reift.kultum.databinding.ActivityMainBinding
 import com.reift.kultum.utils.Transparent
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding as ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Transparent.statusbar(this)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        binding.bottomNav.setupWithNavController(navController)
     }
 }

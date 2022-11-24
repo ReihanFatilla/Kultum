f18b7ce1d7748b02c2d95be11d1579f5aab8b8d1
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 13:53:04 +0700

    [feat]: added connect REgister function

--- core/src/main/java/com/reift/core/domain/usecase/register/RegisterInteractor.kt
+++ core/src/main/java/com/reift/core/domain/usecase/register/RegisterInteractor.kt
@@ -1,4 +1,14 @@
 package com.reift.core.domain.usecase.register
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.User
+
 class RegisterInteractor: RegisterUseCase {
+    override fun checkIfEmailTaken(email: String): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override fun saveUser(user: User) {
+        TODO("Not yet implemented")
+    }
 }
--- core/src/main/java/com/reift/core/domain/usecase/register/RegisterUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/register/RegisterUseCase.kt
@@ -1,4 +1,9 @@
 package com.reift.core.domain.usecase.register
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.User
+
 interface RegisterUseCase {
+    fun checkIfEmailTaken(email: String): LiveData<Boolean>
+    fun saveUser(user: User)
 }

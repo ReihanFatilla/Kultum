e2a702cd8e6d9cf9d749b5eb9fd3f3726087210a
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 13:44:31 +0700

    [feat]: initialize Kultum.kt Model

--- core/src/main/java/com/reift/core/domain/model/Kultum.kt
+++ core/src/main/java/com/reift/core/domain/model/Kultum.kt
@@ -1,3 +1,9 @@
 package com.reift.core.domain.model
 
-data class Kultum()
+data class Kultum(
+    var urlKey: String,
+    var helpful: Int,
+    var comments: List<Comments>,
+    var creator: User,
+    var caption: String
+)

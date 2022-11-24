785f5b745172a7d0309da950abcd646b9caae2f5
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 10:01:27 +0700

    [feat]: added tab bar color logic

--- app/src/main/java/com/reift/kultum/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/reift/kultum/presentation/profile/ProfileFragment.kt
@@ -1,22 +1,56 @@
 package com.reift.kultum.presentation.profile
 
-import androidx.lifecycle.ViewModelProvider
+import android.graphics.PorterDuff
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.reift.kultum.R
+import com.reift.kultum.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
 
     private lateinit var viewModel: ProfileViewModel
 
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding as FragmentProfileBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(layoutInflater)
+
+        setUpTabBar()
+
+        return binding.root
+    }
+
+    private fun setUpTabBar() {
+        binding.profileTab.setOnTabSelectedListener(
+            object : TabLayout.OnTabSelectedListener {
+
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    val tabIconColor =
+                        ContextCompat.getColor(context!!, R.color.primary_color)
+                    tab.icon!!.setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN)
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab) {
+                    val tabIconColor =
+                        ContextCompat.getColor(context!!, R.color.gray_text)
+                    tab.icon!!.setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN)
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab) {
+
+                }
+            }
+        )
     }
 
 }

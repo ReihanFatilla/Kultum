9b56e1b1a9b0fca38c5cc05e1be40c5154d24108
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 13:48:39 +0700

    [feat]: added profile usecases function

--- core/src/main/java/com/reift/core/domain/usecase/profile/ProfileInteractor.kt
+++ core/src/main/java/com/reift/core/domain/usecase/profile/ProfileInteractor.kt
@@ -1,4 +1,19 @@
 package com.reift.core.domain.usecase.profile
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.Kultum
+import com.reift.core.domain.model.User
+
 class ProfileInteractor: ProfileUseCase {
+    override fun getUserDetail(username: String): LiveData<User> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPostedKultum(username: String): LiveData<List<Kultum>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getHelpfulKultum(username: String): LiveData<List<Kultum>> {
+        TODO("Not yet implemented")
+    }
 }
--- core/src/main/java/com/reift/core/domain/usecase/profile/ProfileUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/profile/ProfileUseCase.kt
@@ -1,4 +1,11 @@
 package com.reift.core.domain.usecase.profile
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.Kultum
+import com.reift.core.domain.model.User
+
 interface ProfileUseCase {
+    fun getUserDetail(username: String): LiveData<User>
+    fun getPostedKultum(username: String): LiveData<List<Kultum>>
+    fun getHelpfulKultum(username: String): LiveData<List<Kultum>>
 }

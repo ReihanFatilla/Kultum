2347d855cb2b728af919953b253fabe9bdb13944
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 13:52:00 +0700

    [feat]: added connect usecases function

--- core/src/main/java/com/reift/core/domain/usecase/connect/ConnectUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/connect/ConnectUseCase.kt
@@ -1,4 +1,13 @@
 package com.reift.core.domain.usecase.connect
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.Kultum
+import com.reift.core.domain.model.User
+
 interface ConnectUseCase {
+    fun getUserByUsername(username: String): LiveData<User>
+    fun followUser(user: User)
+    fun unfollowUser(user: User)
+    fun getPostedKultum(username: String): LiveData<List<Kultum>>
+    fun getHelpfulKultum(username: String): LiveData<List<Kultum>>
 }

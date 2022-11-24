79d07fac824803504d8f4a59a9e41130c5678277
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 13:47:31 +0700

    [feat]: added home usecases function

--- core/src/main/java/com/reift/core/domain/usecase/home/HomeInteractor.kt
+++ core/src/main/java/com/reift/core/domain/usecase/home/HomeInteractor.kt
@@ -1,4 +1,23 @@
 package com.reift.core.domain.usecase.home
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.Kultum
+import org.w3c.dom.Comment
+
 class HomeInteractor: HomeUseCase {
+    override fun getKultumForYou(): LiveData<List<Kultum>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addHelpfulKultum(kultum: Kultum) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeHelpfulKultum(kultum: Kultum) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addComment(comment: Comment) {
+        TODO("Not yet implemented")
+    }
 }
--- core/src/main/java/com/reift/core/domain/usecase/home/HomeUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/home/HomeUseCase.kt
@@ -1,4 +1,12 @@
 package com.reift.core.domain.usecase.home
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.Kultum
+import org.w3c.dom.Comment
+
 interface HomeUseCase {
+    fun getKultumForYou(): LiveData<List<Kultum>>
+    fun addHelpfulKultum(kultum: Kultum)
+    fun removeHelpfulKultum(kultum: Kultum)
+    fun addComment(comment: Comment)
 }

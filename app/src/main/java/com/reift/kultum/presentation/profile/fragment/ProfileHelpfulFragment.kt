03fa08370ca02d10a90cd1dbc0db5180f748bbc5
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 10:37:57 +0700

    [feat]: finished Profile Helpful Tab

--- app/src/main/java/com/reift/kultum/presentation/profile/fragment/ProfileHelpfulFragment.kt
+++ app/src/main/java/com/reift/kultum/presentation/profile/fragment/ProfileHelpfulFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.reift.kultum.R
+import com.reift.kultum.adapter.recyclerview.KultumAdapter
 import com.reift.kultum.databinding.FragmentProfileHelpfulBinding
 
 class ProfileHelpfulFragment : Fragment() {
@@ -19,6 +21,33 @@ class ProfileHelpfulFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentProfileHelpfulBinding.inflate(layoutInflater)
+
+        setUpKultum()
+
         return binding.root
+    }
+
+    private fun setUpKultum() {
+        binding.root.apply {
+            val mAdapter = KultumAdapter()
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = mAdapter
+            mAdapter.setKultum(
+                listOf(
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                    R.drawable.img_profile_sample,
+                )
+            )
+        }
     }
 }

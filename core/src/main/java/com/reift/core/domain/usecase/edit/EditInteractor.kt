6c7c92d60f47175c82bc25604ff9ab0f0d52df31
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 13:55:53 +0700

    [feat]: added edit usecases function

--- core/src/main/java/com/reift/core/domain/usecase/edit/EditInteractor.kt
+++ core/src/main/java/com/reift/core/domain/usecase/edit/EditInteractor.kt
@@ -1,4 +1,13 @@
 package com.reift.core.domain.usecase.edit
 
+import androidx.lifecycle.LiveData
+
 class EditInteractor: EditUseCase {
+    override fun saveEditedProfile(username: String, name: String, bio: String, photoUrl: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun checkIfUserTaken(username: String): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
 }
--- core/src/main/java/com/reift/core/domain/usecase/edit/EditUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/edit/EditUseCase.kt
@@ -1,4 +1,9 @@
 package com.reift.core.domain.usecase.edit
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.User
+
 interface EditUseCase {
+    fun saveEditedProfile(username: String, name: String, bio: String, photoUrl: String)
+    fun checkIfUserTaken(username: String): LiveData<Boolean>
 }

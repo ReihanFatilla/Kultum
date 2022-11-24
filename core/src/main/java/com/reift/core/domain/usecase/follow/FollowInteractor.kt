ad42efb48b1998d57e677c055ca4afd524626344
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-24 13:49:33 +0700

    [feat]: added follow usecases function

--- core/src/main/java/com/reift/core/domain/usecase/follow/FollowInteractor.kt
+++ core/src/main/java/com/reift/core/domain/usecase/follow/FollowInteractor.kt
@@ -1,4 +1,14 @@
 package com.reift.core.domain.usecase.follow
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.User
+
 class FollowInteractor: FollowUseCase {
+    override fun getFollowers(username: String): LiveData<List<User>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getFollowings(username: String): LiveData<List<User>> {
+        TODO("Not yet implemented")
+    }
 }
--- core/src/main/java/com/reift/core/domain/usecase/follow/FollowUseCase.kt
+++ core/src/main/java/com/reift/core/domain/usecase/follow/FollowUseCase.kt
@@ -1,4 +1,9 @@
 package com.reift.core.domain.usecase.follow
 
+import androidx.lifecycle.LiveData
+import com.reift.core.domain.model.User
+
 interface FollowUseCase {
+    fun getFollowers(username: String): LiveData<List<User>>
+    fun getFollowings(username: String): LiveData<List<User>>
 }

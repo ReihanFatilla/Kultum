e288c209fe2d12b3e5b1f636c2ed13a612b3db87
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-25 07:28:08 +0700

    [feat]: added Register Repository Implementation Function

--- core/src/main/java/com/reift/core/data/repository/RegisterRepositoryImpl.kt
+++ core/src/main/java/com/reift/core/data/repository/RegisterRepositoryImpl.kt
@@ -1,17 +1,50 @@
 package com.reift.core.data.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.reift.core.constant.Pref
+import com.reift.core.constant.Ref
+import com.reift.core.data.source.local.LocalDataSource
+import com.reift.core.data.source.remote.FirebaseDataSource
 import com.reift.core.domain.model.User
 import com.reift.core.domain.repository.RegisterRepository
 
 class RegisterRepositoryImpl(
-
+    val firebaseDataSource: FirebaseDataSource,
+    val localDataSource: LocalDataSource
 ): RegisterRepository {
+
     override fun checkIfEmailTaken(email: String): LiveData<Boolean> {
-        TODO("Not yet implemented")
+        val isTaken = MutableLiveData<Boolean>()
+        firebaseDataSource.getReference(Ref.USER).addValueEventListener(
+            object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for(i in snapshot.children){
+                        val user = i.getValue(User::class.java)
+                        if(user?.email == email) continue
+                        isTaken.value = false
+                        break
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+        )
+        return isTaken
     }
 
     override fun saveUser(user: User) {
-        TODO("Not yet implemented")
+        firebaseDataSource.getReference(Ref.USER)
+            .child(user.usernname)
+            .setValue(user)
+
+        localDataSource.add(Pref.CURRENT_USER, user.usernname)
+        localDataSource.add(Pref.IS_USER_LOGIN, true)
     }
 }

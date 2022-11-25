41aefe4eb1ae5657dca1fe00f3870d0cb0bd35a2
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-25 09:18:50 +0700

    [feat]: added RepositoryModule.kt

---
+++ core/src/main/java/com/reift/core/di/RepositoryModule.kt
@@ -0,0 +1,16 @@
+package com.reift.core.di
+
+import com.reift.core.data.repository.*
+import com.reift.core.domain.repository.*
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    single<ConnectRepository> { ConnectRepositoryImpl(get(), get()) }
+    single<EditRepository> { EditRepositoryImpl(get(), get()) }
+    single<FollowRepository> { FollowRepositoryImpl(get()) }
+    single<HomeRepository> { HomeRepositoryImpl(get(), get()) }
+    single<LoginRepository> { LoginRepositoryImpl(get(), get()) }
+    single<ProfileRepository> { ProfileRepositoryImpl(get(), get()) }
+    single<RegisterRepository> { RegisterRepositoryImpl(get(), get()) }
+    single<SplashRepository> { SplashRepositoryImpl(get()) }
+}

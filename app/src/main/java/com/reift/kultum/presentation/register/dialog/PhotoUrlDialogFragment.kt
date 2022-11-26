707c733c233bf45c46bbea163d4b5973b20bed56
ReihanFatilla <reihanfatilla@gmail.com>
2022-11-26 22:24:13 +0700

    [feat]: added Photo Url Dialog Fragment

---
+++ app/src/main/java/com/reift/kultum/presentation/register/dialog/PhotoUrlDialogFragment.kt
@@ -0,0 +1,49 @@
+package com.reift.kultum.presentation.register.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.reift.kultum.R
+import com.reift.kultum.databinding.ActivityEditBinding
+import com.reift.kultum.databinding.FragmentPhotoUrlDialogBinding
+
+class PhotoUrlDialogFragment : DialogFragment() {
+
+    private var _binding: FragmentPhotoUrlDialogBinding? = null
+    private val binding get() = _binding as FragmentPhotoUrlDialogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPhotoUrlDialogBinding.inflate(layoutInflater)
+        setUpView()
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    private fun setUpView() {
+        binding.edtImageUrl.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    companion object {
+
+        const val TAG = "DialogWithData"
+
+    }
+
+
+}
